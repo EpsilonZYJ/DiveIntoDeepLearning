9fae784ba6f02122ee0171ce80762e1beefaeb0f
Yujie Zhou <143871992+EpsilonZYJ@users.noreply.github.com>
2025-02-07 21:28:57 +0800

    L6

--- Utils/__init__.py
+++ Utils/__init__.py
@@ -1,1 +1,1 @@
-from .fashion_minist import load_data_fashion_mnist, train_ch3
+from .fashion_minist import *
--- Utils/fashion_minist/__init__.py
+++ Utils/fashion_minist/__init__.py
@@ -1,2 +1,6 @@
 from .data import load_data_fashion_mnist
+
 from .train import train_ch3
+from .train import Accumulator
+from .train import Animator
+from .train import train_epoch_ch3
