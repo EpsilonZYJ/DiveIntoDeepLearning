fb120e174d44091dfe53cbdde81a4025f19bfb20
EpsilonZYJ <1425315901@qq.com>
2025-02-19 09:37:47 +0800

    RNN

--- Utils/LanguageModel/Preprocess.py
+++ Utils/LanguageModel/Preprocess.py
@@ -1,6 +1,19 @@
 import re
 from d2l import torch as d2l
 
+class SeqDataLoader:  #@save
+    """加载序列数据的迭代器"""
+    def __init__(self, batch_size, num_steps, use_random_iter, max_tokens):
+        if use_random_iter:
+            self.data_iter_fn = d2l.seq_data_iter_random
+        else:
+            self.data_iter_fn = d2l.seq_data_iter_sequential
+        self.corpus, self.vocab = d2l.load_corpus_time_machine(max_tokens)
+        self.batch_size, self.num_steps = batch_size, num_steps
+
+    def __iter__(self):
+        return self.data_iter_fn(self.corpus, self.batch_size, self.num_steps)
+
 def read_time_machine():  #@save
     d2l.DATA_HUB['time_machine'] = (d2l.DATA_URL + 'timemachine.txt',
                                 '090b5e7e70c295757f55df93cb0a180b9691891a')
@@ -8,3 +21,10 @@ def read_time_machine():  #@save
     with open(d2l.download('time_machine'), 'r') as f:
         lines = f.readlines()
     return [re.sub('[^A-Za-z]+', ' ', line).strip().lower() for line in lines]
+
+def load_data_time_machine(batch_size, num_steps,  #@save
+                           use_random_iter=False, max_tokens=10000):
+    """返回时光机器数据集的迭代器和词表"""
+    data_iter = SeqDataLoader(
+        batch_size, num_steps, use_random_iter, max_tokens)
+    return data_iter, data_iter.vocab
--- Utils/LanguageModel/__init__.py
+++ Utils/LanguageModel/__init__.py
@@ -1,1 +1,2 @@
 from .Preprocess import read_time_machine
+from .Preprocess import load_data_time_machine
